5b7f036420a1948ab6fa40250dc9b888e5d88469
woshiren <yejiacong@outlook.com>
2026-03-04 18:54:17 +0800

    add debug log

--- src/Example/example_main.cpp
+++ src/Example/example_main.cpp
@@ -10,6 +10,7 @@ int main()
     asio::io_context io_context;
     Interrupt::Controller controller(0x10000000, 0x1000, 1);
     auto fds = Interrupt::InterruptNodeFinder::find("/dev/xdma0_events_");
+    std::cout << "Found " << fds.size() << " interrupt nodes." << std::endl;
     auto listener =
         Interrupt::Listener::create(std::move(fds), controller, io_context);
     listener->registerHandler(
